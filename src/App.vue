c48a51c490c1a1af53376bded1af89a73db61612
June Rockwell <junerockwell@Junes-iMac.local>
2017-03-15 21:35:10 -0700

    First commit

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+<div id="wrapper" class="container"> 
+ <modal v-if="showModal"> 
+  <h3 slot="header" class="modal-title">
+    Modal title
+  </h3>
+  
+  <div slot="body">
+    <img src="./assets/logo.png" />
+  </div>
+
+  <div slot="footer">
+   <button type="button" class="btn btn-outline-info" @click="closeModal()"> Close </button>
+   <button type="button" class="btn btn-primary" data-dismiss="modal" @click="submitAndClose()">
+     Submit
+   </button>
+  </div>
+</modal>
+  <button type="button" class="btn btn-primary" @click="openModal()">Open Modal</button>
+ </div>
+</template>
+
+<script>
+// import it
+import Modal from './components/Modal';
+
+export default {
+  // register it as a component
+  components: {
+    Modal
+  }, 
+  data() { 
+    return { 
+      showModal: false 
+    } 
+  }, 
+ methods: { 
+   openModal() { 
+     this.showModal = true; 
+   } ,
+  closeModal() {
+     this.showModal = false;
+  },
+  submitAndClose() {
+  
+  }
+ }  
+}
+</script>
+
+<style>
+#wrapper {
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,35 @@
+<template> 
+<transition name="modal">
+ <div class="modal modal-mask" style="display: block">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <slot name="header"></slot>
+      </div>
+      
+      <div class="modal-body">
+        <slot name="body"></slot>
+      </div>
+      
+      <div class="modal-footer">
+        <slot name="footer"></slot>
+      </div>
+    </div>
+  </div>
+</div>
+</transition> 
+</template>
+
+<style>
+.modal-mask {
+ position: fixed;
+ z-index: 9998;
+ top: 0;
+ left: 0;
+ width: 100%;
+ height: 100%;
+ background-color: rgba(0, 0, 0, .5);
+ display: table;
+ transition: opacity .3s ease;
+}
+</style>
